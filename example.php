4a75edc872c61f60354965c26281aa96d518d35d
breakermind <hello@breakermind.com>
2017-12-03 11:03:27 +0100

    Blastex - Php Ssl Smtp Email Client
    Blastex - Php ssl smtp email client (Send e-mail message without smtp server). Works on outlook.com, hotmail.com, gmail.com, yahoo.com, ovh.com (tested)

--- example.php
+++ example.php
@@ -10,42 +10,42 @@ $m = new Blastex();
 // Show logs
 $m->Debug(1);
 
-// hello hostname
-$m->addHeloHost("qflash.pl");
+// hello hostname (your vps ip hostname)
+$m->addHeloHost("domain.xx");
 
 // Add from
-$m->addFrom("info@qflash.pl", "Ania Bania");
+$m->addFrom("jim@domain.xx", "Ania Bania");
 
 // Add to
-$m->addTo("xxx@breakermind.com", "Albercik Kutafonek");
-$m->addTo("xxx@gmail.com", "Adela Mela");
+$m->addTo("albert@hotmail.com", "Albert");
+$m->addTo("adzia@gmail.com", "Adela Mela");
 
 // Add Cc
-$m->addCc("zzz@gmail.com", "Ben");
-$m->addCc("ccc@yahoo.com");
-$m->addCc("aaa@outlook.com");
+$m->addCc("ben@gmail.com", "Ben Pen");
+$m->addCc("max@yahoo.com");
+$m->addCc("asy@outlook.com");
 
 // Add Bcc
 $m->addBcc("boos@domain.com", "BOSS");    
 
 $m->addText("Hello message");
 
-$m->addHtml("<h1>Hello message html</h1>");
+$m->addHtml('<h1>Hello message html</h1> My photo <img src="cid:photo.zenek123">');
 
 $m->addSubject("Hello from smtp email client !!!");
 
 // Add files inline
-// $m->addFile('photo.jpg',"zenek123");
+$m->addFile('photo.jpg',"photo.zenek123");
 
 // Add file
-// $m->addFile('sun.png');
+$m->addFile('sun.png');
 
 // Send email from dns mx hosts
 $m->Send();
 echo $m->lastError;
 
 // Create mime message: $msgText, $msgHtml, $subject, $fromName, $fromEmail, $replyTo
-// $m->createMime("Witaj księżniczko Alabambo",'<h1>Witaj księżniczko Alabambo <img src="cid:zenek123"> </h1>',"Wesołych świąt życzę!","Heniek Wielki", "heniek@domain.com");
+// $m->createMime("Witaj Maxiu",'<h1>Witaj Maxiu <img src="cid:zenek123"> </h1>',"Wesołych świąt życzę!","Heniek Wielki", "jim@domain.xx");
 
 // get mime
 // $m->getMime();
